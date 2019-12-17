0637b42ae7793d7dc1cd863d88fcae0b8f7f8b39
Martin Balke <Mrtnbalke@gmail.com>
2019-12-17 14:19:09 -0800

    commenting

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import '../styles/styles.scss';
 
+/**
+ * Converter is a react component class that allows us to use event handlers and 
+ * the render function to dynamically display HMTL to the page
+ * @class
+ */
 class Converter extends React.Component {
+    /**
+     * @constructor
+     * @param {Object} props - Props are passed to this component constructor to control the rate
+     * @param {Object} this.state = X and Y properties used to control the rate of change
+     */
     constructor(props) {
         super(props);
         this.state = {
@@ -10,11 +20,17 @@ class Converter extends React.Component {
             rate: this.props.rate ? this.props.rate : 1,
         };
     }
-
+    /**
+     * Roundit tkes in a number and rounds it
+     * @param {Number} num - The number we are trying to round
+     */
     roundIt(num) {
         return Math.round(num * 1000) / 1000;
     }
-
+    /**
+     * HandleChange is an event handler that allows us to dynamically display the conversion information based on a user's input
+     * @param {Object} e - The event object. Used in this case to grab ID and Value from an input field
+     */
     handleChange(e) {
         if (!e.target.value) {
             this.setState({ x: '', y: '' });
@@ -33,7 +49,10 @@ class Converter extends React.Component {
                 y: value,
             });
     }
-
+    /**
+     * Render allows us to render our JSX to the page dynamically
+     * @returns {HTML}
+     */
     render() {
         let xLabel = this.props.xLabel ? this.props.xLabel : '1';
         let yLabel = this.props.yLabel || '1';
--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+//Render app to the page at the root element
 ReactDOM.render(<App />, document.getElementById('root'));
